24cbdc95290a347301c36f33dce87b98b2e729c3
PKT <rudxorrmrwkd@gmail.com>
2025-05-21 11:39:22 +0900

    플레이어 움직임, 카메라회전, 점프 구현 완료

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -7,30 +7,26 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public LayerMask groundlayer;
     [SerializeField]
     private float moveSpeed;
-    private Vector2 curMovementDir; //방향 입력
+    private Vector2 curMoveMentInput; //방향 입력
 
     [SerializeField]
     private float jumpForce;
+    private Vector2 mouseDelta;
 
     [Header("Camera")]
     [SerializeField]
     private Transform cameraContainer;
-    private float minLookX;
-    private float maxLookX;
+    private float minLookX = -90f;
+    private float maxLookX = 90f;
     private float curCamRotX;
-
-
-
-
-    private Vector2 MouseDelta;//마우스 이동량
-
-
-    private LayerMask groundlayer;
-
+    public float lookSensitivity; //마우스 감도조절
 
     Rigidbody rigid;
+
+
 
 
     private void Start()
@@ -39,11 +35,24 @@ public class PlayerController : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
     }
 
+    private void FixedUpdate()
+    {
+        MoveMent();
+    }
+    private void LateUpdate()
+    {
+        CameraLook();
+        Debug.DrawRay(transform.position + (transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down);
+
+    }
+
+
+
     void MoveMent()
     {
 
-        Vector3 moveDir = transform.forward * curMovementDir.y + transform.right * curMovementDir.x;
-        moveDir *= moveSpeed * Time.deltaTime;
+        Vector3 moveDir = transform.forward * curMoveMentInput.y + transform.right * curMoveMentInput.x;
+        moveDir *= moveSpeed;
         moveDir.y = rigid.velocity.y;
 
 
@@ -52,42 +61,65 @@ public class PlayerController : MonoBehaviour
     }
 
 
-//이거랑 같은거 아닌가요??아래
     public void OnMove(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Performed)
         {
 
-            curMovementDir = context.ReadValue<Vector2>();
+            curMoveMentInput = context.ReadValue<Vector2>();
 
         }
         else
         {
 
-            curMovementDir = Vector2.zero; //안움직임
+            curMoveMentInput = Vector2.zero; //안움직임
         }
 
     }
 
-    void JumpInput(InputAction.CallbackContext context)
+
+    public void OnLookInput(InputAction.CallbackContext context)
     {
-        if (context.phase == InputActionPhase.Performed)
+        mouseDelta = context.ReadValue<Vector2>(); //마우스 이동량
+    }
+
+    private void CameraLook()
+    {
+        curCamRotX += mouseDelta.y * lookSensitivity;
+       
+        curCamRotX = Mathf.Clamp(curCamRotX,minLookX,maxLookX); //범위 제한
+        cameraContainer.localEulerAngles = new Vector3(-curCamRotX, 0, 0);
+        
+
+        transform.eulerAngles += new Vector3(0,mouseDelta.x * lookSensitivity, 0); //카메라 회전
+
+    }
+
+
+    public void JumpInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
         {
-            if (Physics.Raycast(transform.position, Vector3.down, 1.1f, groundlayer))
-            {
-                rigid.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+            if (IsGrounded()) //여기에 추가로 버프를 먹으면 
+            { 
+           rigid.AddForce(Vector2.up * jumpForce, ForceMode.Impulse);
             }
+          
         }
     }
+
+
+ 
 
     private bool IsGrounded()
     {
         Ray[] ray = new Ray[4]
         {
           new Ray(transform.position + (transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down),
-            new Ray(transform.position + (-Vector3.forward * 0.2f)+(transform.up*0.01f),Vector3.down),
-            new Ray(transform.position + (Vector3.right* 0.5f) +(transform.up* 0.01f) ,Vector3.down),
-            new Ray(transform.position + (-Vector3.right* 0.5f) +(transform.up* 0.01f) ,Vector3.down)
+            new Ray(transform.position + (-transform.forward * 0.2f)+(transform.up*0.01f),Vector3.down),
+            new Ray(transform.position + (transform.right* 0.2f) +(transform.up* 0.01f) ,Vector3.down),
+            new Ray(transform.position + (-transform.right* 0.2f) +(transform.up* 0.01f) ,Vector3.down)
 
 
         };
