654679abf33ea8d02f5b31a41ef61ff0b343c46b
PKT <rudxorrmrwkd@gmail.com>
2025-05-22 00:00:21 +0900

    아이템 SO 변경

--- Assets/Script/ScriptableObject/ItemData.cs
+++ Assets/Script/ScriptableObject/ItemData.cs
@@ -1,28 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+public enum ItemType
+{
+    Consumable,
+    Resource
+}
+public enum ItemMesh
+{
+    None,
+    Sphere,
+    Square
+}
 
+
+public enum BuffType
+{
+    Sphere,
+    Square
+}
+[System.Serializable]
+public class ItemDataConsumable
+{
+    public BuffType type;
+    public float value;
+};
 
 [CreateAssetMenu(fileName = "Items", menuName = "ScriptableObject/Items")]
 public class ItemData : ScriptableObject
 {
-    public enum ItemType
-    { 
-        Consumable,
-        Resource
-    }
-
-    public enum ItemMesh
-    {
-        Sphere,
-        Square
-    }
-
-    public Mesh itemMesh;
-
+    [Header("Info")]
     public string ItemName;
     public string ItemInfo;
+    public ItemType type;
+    public Sprite icon;
+    public GameObject dropPrefabs;
+
+    [Header("Stacking")]
+    public bool canStack;
+    public int maxStackAmount;
+
+    [Header("Consumable")]
+    public ItemDataConsumable[] consumables;
+
+    [Header("Mesh Change")]
+    public ItemMesh meshType = ItemMesh.None;
+    public Mesh applyMesh;
 
 
-    
+
+
+
+
+
+
 }
